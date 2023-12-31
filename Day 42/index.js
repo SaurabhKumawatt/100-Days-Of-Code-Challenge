0b97e26af44998ac818de317664f16718c785ac9
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2023-12-31 23:40:11 +0530

    Day 42 Completed

---
+++ Day 42/index.js
@@ -0,0 +1,34 @@
+console.log("Day 42 - 100 Days Of Code Challenge");
+
+// js excute code and call stack
+
+//control flow => for building logic
+// if
+if (10 > 5){
+    console.log("10 is grater than 5");
+}
+
+// const score = 1000
+
+// if (score <= 100){
+//     console.log("you level is bronze");
+// } else{
+//     console.log("you level is silver");
+// }
+
+//shortnoted if
+// let a = 10
+// let b = 15
+// if (a < b) console.log(`${a} is less than ${b}`);
+
+// multiple condition
+const level = 132
+if (level <= 5){
+    console.log("your level is bronze");
+} else if(level <= 10){
+    console.log("your level is silver");
+} else if(level <= 15){
+    console.log("your level is gold");
+} else {
+    console.log("your level is diamond");
+}
