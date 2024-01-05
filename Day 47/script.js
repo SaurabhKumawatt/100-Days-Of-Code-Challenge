350d4ebe9ce9bfc7f28241f67163a9f916329e01
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-01-06 00:04:00 +0530

    Day 47 Completed

---
+++ Day 47/script.js
@@ -0,0 +1,11 @@
+document.getElementById("name").innerHTML = "<h1>Saurabh Kumawat</h1>"
+
+const dis = document.getElementById("dis")
+dis.innerHTML ="<p>JavaScript is the world's most popular programming language.</p>"
+
+dis.style.color = "red"
+dis.style.fontSize = "30px"
+
+
+const newTitle = document.getElementById('title')
+newTitle.innerHTML = "<h3>Day 47 - 100 Days Of Code Challenge</h3>"
