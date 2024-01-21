a9e63121598a64d3845c3ee52478e04581f3ba1d
Saurabh Kumawat <89497538+SaurabhKumawatt@users.noreply.github.com>
2024-01-22 02:41:54 +0530

    DAy 63 Completed

---
+++ Day 62/spotify-clone/script.js
@@ -0,0 +1,22 @@
+let currentSong = new Audio();
+let songs;
+let currFolder;
+
+// function to get all songs
+async function getSongs(folder){
+    currFolder = folder;
+    let a = await fetch(`/${folder}/`)
+    let response = await a.text();
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${folder}/`)[1])
+        }
+    }
+    console.log(response);
+}
+getSongs()
